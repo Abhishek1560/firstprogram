e7919b7fce49e6b463c5a7c4d61b4da3744f54fd
Abhishek1560 <94213828+Abhishek1560@users.noreply.github.com>
2021-11-15 16:53:04 +0530

    Add Two Strings
    Add firstname and lastname in string

---
+++ Twostrings.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()     {
+string fullname, firstname,lastname;
+cout<< " Enter your firstname: ";
+getline(cin,firstname);
+cout<< " Enter your lastname: ";
+getline(cin,lastname);
+fullname = firstname +" "+ lastname;
+cout<< " My fullname is: "<<fullname<<endl;
+return 0;
+}
